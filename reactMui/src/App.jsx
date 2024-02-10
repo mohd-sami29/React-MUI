f9ceeeaf4a4f13fda1b96248a78f64e35a97941a
MD SAMIULLAH <sami.mohd29@gmail.com>
2024-02-10 18:07:05 +0530

    first commit

---
+++ reactMui/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import "./App.css";
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>React-MUI</h1>
+
+      <Card variant="outlined" sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.success" gutterBottom>
+            Word of the Day
+          </Typography>
+          <Typography variant="h5" component="div">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.success">
+            adjective
+          </Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+
+      {/* <Button variant="text">Text</Button> */}
+    </>
+  );
+}
+
+export default App;
